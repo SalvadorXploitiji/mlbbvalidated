51971c9a6855e602502ed6912ebc44382425efb3
Mr SalVador <brenzxgacor@gmail.com>
2025-04-19 19:52:41 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,21 @@
+module.exports = {
+  orderChannels: ['Google', 'Apple', 'Coda', 'Razer', 'Unipin', 'Other'],
+  
+  devices: [
+    'iPhone 12', 'iPhone 13', 'iPhone 14 Pro Max', 'iPhone XR',
+    'Samsung Galaxy S22', 'Samsung Galaxy S21 FE', 'Samsung Galaxy A73',
+    'Redmi Note 10', 'Redmi Note 11 Pro', 'Redmi 12C',
+    'POCO X3 Pro', 'POCO M4 Pro', 'POCO F3',
+    'Realme 8', 'Realme Narzo 50', 'Realme C55',
+    'Oppo A76', 'Oppo Reno8 T', 'Oppo F19 Pro',
+    'Vivo Y21', 'Vivo V25e', 'Vivo Y36',
+    'Huawei Nova 9', 'Huawei Y9 Prime', 'Huawei Mate 20',
+    'Infinix Zero 5G', 'Infinix Note 12', 'Infinix Hot 30',
+    'Asus ROG Phone 5', 'Asus Zenfone 9',
+    'Nothing Phone 1', 'Lenovo Legion Duel 2',
+    'Sony Xperia 1 III', 'Nokia X20',
+    'ZTE Nubia RedMagic 7'
+  ],
+
+  thirdParty: ['Facebook', 'Google', 'TikTok', 'Gamecenter', 'VK', 'Telegram']
+};
